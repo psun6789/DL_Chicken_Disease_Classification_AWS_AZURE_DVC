92766e852e0c2663e192916400c1658b3ae26e2a
psun6789 <psunacademy@gmail.com>
2024-05-27 13:22:29 +0100

    User APP

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS, cross_origin
+from ImageClassification.utils.common import decodeImage
+from ImageClassification.pipeline.predict import PredictionPipeline
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+CORS(app)
+
+class ClientApp:
+    def __init__(self):
+        self.filename = 'inputImage.jpg'
+        self.classifier = PredictionPipeline(self.filename)
+    
+    @app.route('/', methods=['GET'])
+    @cross_origin()
+    def home():
+        return render_template('index.html')
+    
+    @app.route('/train', methods=['GET', 'POST'])
+    @cross_origin()
+    def trainRout():
+        os.system('Python main.py')
+        # os.system('dvc repro') # Use this if you are using dvc to run 
+        return 'Training Done Successfully'
+    
+    @app.route('/predict', methods=['POST'])
+    @cross_origin()
+    def predictionRout():
+        image = request.json['image']
+        decodeImage(image, clApp.filename)
+        result = clApp.classifier.predict()
+        return jsonify(result)
+    
+if __name__ == '__main__':
+    clApp = ClientApp()
+    app.run(host='0.0.0.0', port=8080) #local host
+    # app.run(host='0.0.0.0', port=8080) #for AWS
+    # app.run(host='0.0.0.0', port=80) #for AZURE
